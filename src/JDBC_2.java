b17c8abbb0156170b83eda25091407e468be7782
Ganesh <ganeshpodeti6@gmail.com>
2023-10-17 13:48:02 +0530

    inserting data into the database

--- src/JDBC_2.java
+++ src/JDBC_2.java
@@ -4,7 +4,7 @@ import java.sql.*;
 public class JDBC_2 {
     public static void main(String[] args) {
         try{
-            Class.forName("com.mysql.cj.jdbc.Driver");
+//            Class.forName("com.mysql.cj.jdbc.Driver");
             String url = "jdbc:mysql://localhost:3306/myTable";
             String username = "root";
             String password = "studyganesh";
--- src/JDBC_3.java
+++ src/JDBC_3.java
@@ -5,14 +5,27 @@ import java.sql.*;
 public class JDBC_3 {
     public static void main(String[] args) {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            String url = "jdbc:mysql://localhost:3306/learning_jdbc";
+//            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:3306/myTable";
             String username = "root";
             String password = "studyganesh";
             Connection con = DriverManager.getConnection(url, username, password);
 
             //Creating a query
-            String q = "INSERT INTO learning_jdbc";
+            String q = "INSERT INTO myTable (tName, tCity) values (?,?)";
+
+            //getting the prepared statement object
+            PreparedStatement pstmt = con.prepareStatement(q);
+//            pstmt.setString(1,"Satish");
+//            pstmt.setString(2, "Mumbai");
+//            pstmt.setString(1,"Sonu");
+//            pstmt.setString(2, "HYD");
+            pstmt.setString(1,"Vignesh");
+            pstmt.setString(2, "Pune");
+            pstmt.executeUpdate();
+            System.out.println("Data inserted in the table");
+            con.close();
+
         }catch(Exception e){
             System.out.println("Some Exception occur "+e);
         }
